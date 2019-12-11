3976b15d47897534b80fc8339eaba040e094c207
QianDashuai <491787891@qq.com>
2019-12-11 19:42:11 +0800

    [add]增加部分页面

---
+++ app/src/main/java/com/heiguo/blackfruitvip/ui/ForgetActivity.java
@@ -0,0 +1,18 @@
+package com.heiguo.blackfruitvip.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.heiguo.blackfruitvip.R;
+import com.heiguo.blackfruitvip.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_forget)
+public class ForgetActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/heiguo/blackfruitvip/ui/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.heiguo.blackfruitvip.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.heiguo.blackfruitvip.R;
+import com.heiguo.blackfruitvip.base.BaseActivity;
+
+import org.xutils.view.annotation.ContentView;
+
+@ContentView(R.layout.activity_register)
+public class RegisterActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
